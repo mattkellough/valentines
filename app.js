5640cc5c7885e590da4c0f926e5bcd7ea1767410
Matt Kellough <mrk10a@gmail.com>
2017-02-13 19:42:42 -0500

    centered

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ $(document).ready(function(){
 		var $self = $(this);
 		$self.find('.envelope').removeClass('tossing').addClass('open');
 		$self.find('.heart use').attr("xlink:href", "#icon-heart-broken");
+		$('.js-open-envelope').animate({transform:'scale(0)'});  
 	});
 });
