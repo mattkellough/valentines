731993693f9c5acc39ede744963550f6bd65925b
Matt Kellough <mrk10a@gmail.com>
2017-02-13 19:00:06 -0500

    valentines

---
+++ app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.js-open-envelope').one('click', function (event) {
+		event.preventDefault();
+		var $self = $(this);
+		$self.find('.envelope').removeClass('tossing').addClass('open');
+		$self.find('.heart use').attr("xlink:href", "#icon-heart-broken");
+	});
+});
